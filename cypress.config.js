dff59dff146a9578da8a893f4e68f5f113cd9340
Adem Polat <polatadem.2541@gmail.com>
2022-11-16 13:28:52 +0100

    cypress cucumber struktur

---
+++ cypress.config.js
@@ -0,0 +1,17 @@
+//const { defineConfig } = require("cypress");
+
+//module.exports = defineConfig({
+  //e2e: {
+    //setupNodeEvents(on, config) {
+      // implement node event listeners here
+    //},
+  //},
+//});
+const cucumber =require('cypress-cucumber-preprocessor').default
+
+module.exports =(on,config)=>{
+    on('file:prepprocessor',cucumber())
+},
+{
+  "testFiles":"**/*.feature"
+}
